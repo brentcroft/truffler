b620312c46b51b2ad7cdcd958b2da666b541a908
tul-urte <tul_urte@brentcroft.com>
2019-07-02 22:45:32 +0100

    session-009 retry file deletes

--- src/main/java/com/brentcroft/trufflehog/util/Local.java
+++ src/main/java/com/brentcroft/trufflehog/util/Local.java
@@ -1,10 +1,22 @@
 package com.brentcroft.trufflehog.util;
 
+import lombok.extern.java.Log;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 import java.util.function.Function;
+import java.util.logging.Level;
 
+import static java.lang.String.format;
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+@Log
 public class Local
 {
     public static List< String > getFileLines( String uri )
@@ -18,8 +30,7 @@ public class Local
             try( Scanner scanner = new Scanner( is ) )
             {
                 return scanner.useDelimiter( "\\Z" ).next();
-            }
-            catch (NoSuchElementException e )
+            } catch( NoSuchElementException e )
             {
                 return "";
             }
@@ -30,7 +41,7 @@ public class Local
     {
         try( InputStream is = Local.class.getClassLoader().getResourceAsStream( uri ) )
         {
-            if ( Objects.isNull(is))
+            if( Objects.isNull( is ) )
             {
                 throw new IllegalArgumentException( "No such resource: " + uri );
             }
@@ -39,6 +50,68 @@ public class Local
         } catch( IOException e )
         {
             throw new IllegalArgumentException( e );
+        }
+    }
+
+    public static void removeAll( Path path )
+    {
+        if( Objects.nonNull( path ) )
+        {
+            boolean completed = false;
+            int maxAttempts = 10;
+            int attempts = 0;
+
+            while( ! completed && attempts <= maxAttempts )
+            {
+                attempts++;
+
+                try
+                {
+                    Files.walkFileTree( path, new Remover() );
+
+                    completed = true;
+
+                    log.log( Level.INFO, format( "Emptied directory after [%s] attempts: %s", attempts, path ) );
+
+                } catch( Exception e )
+                {
+                    try
+                    {
+                        Thread.sleep( 1 );
+                    } catch( InterruptedException e1 )
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    static class Remover extends SimpleFileVisitor< Path >
+    {
+        @Override
+        public FileVisitResult visitFile( Path file, BasicFileAttributes attr )
+        {
+            if( attr.isRegularFile() )
+            {
+                if( ! file.toFile().delete() )
+                {
+                    throw new IllegalStateException( "Failed to delete file: " + file );
+                }
+            }
+
+            return CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory( Path dir, IOException exc )
+        {
+            if( ! dir.toFile().delete() )
+            {
+                throw new IllegalStateException( "Failed to delete directory: " + dir );
+            }
+
+            return CONTINUE;
         }
     }
 }
