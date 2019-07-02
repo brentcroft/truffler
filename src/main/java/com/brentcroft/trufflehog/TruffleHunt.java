f718e7a63427543330e01dfddc3dad9389bbbfcc
tul-urte <tul_urte@brentcroft.com>
2019-07-02 22:48:13 +0100

    session-008 uncomment default config

--- src/main/java/com/brentcroft/trufflehog/TruffleHunt.java
+++ src/main/java/com/brentcroft/trufflehog/TruffleHunt.java
@@ -8,16 +8,16 @@ public class TruffleHunt
 {
     protected DefaultTruffler truffler = new DefaultTruffler();
 
-//    @Before
-//    public void configure()
-//    {
-//        truffler.getEntropySniffer().setEntropyThreshold( "b64", 4.5 );
-//        truffler.getEntropySniffer().setEntropyThreshold( "hex", 3.0 );
-//
-//        truffler.getRegexSniffer().withRegex( "ld", "la[z]ydog" );
-//
-//        truffler.getXmlReceiver().withIssueTextNodes( true );
-//    }
+    @Before
+    public void configure()
+    {
+        truffler.getEntropySniffer().setEntropyThreshold( "b64", 4.5 );
+        truffler.getEntropySniffer().setEntropyThreshold( "hex", 3.0 );
+
+        truffler.getRegexSniffer().withRegex( "ld", "la[z]ydog" );
+
+        truffler.getXmlReceiver().withIssueTextNodes( true );
+    }
 
     @Test
     public void truffle()
