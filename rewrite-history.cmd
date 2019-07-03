a8362e94ce7a9e180d8e61b75e56113ca07ab42d
tul-urte <tul_urte@brentcroft.com>
2019-07-03 22:41:48 +0100

    session-012 refine script

--- rewrite-history.cmd
+++ rewrite-history.cmd
@@ -5,28 +5,26 @@ set -e
 :: https://rtyley.github.io/bfg-repo-cleaner/
 :: https://stackoverflow.com/questions/12450245/getting-a-working-copy-of-a-bare-repository
 
-set git_url=file://e:/project/truffler/.git
-set BFG=java -jar bfg-1.13.0.jar
+set project_name=truffler
+set git_url=file://e:/project/%project_name%/.git
 
-::
+set BFG=java -jar bfg-1.13.0.jar
 set replace_file=strings-to-replace.txt
 
 if exist %replace_file% (
 
     git clone --mirror %git_url%
 
-    %BFG% --replace-text %replace_file% truffler.git
+    %BFG% --replace-text %replace_file% %project_name%.git
 
-    cd truffler.git
+    cd %project_name%.git
 
     git reflog expire --expire=now --all
     git gc --prune=now --aggressive
 
     cd ..
 
-    git clone truffler.git
-
+    git clone %project_name%.git
 ) else (
     echo "No strings to replace %replace_file%"
 )
-
