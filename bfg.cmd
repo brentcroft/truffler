a301a1b6ec8e636a0217db2a0092027684e7fb08
tul-urte <tul_urte@brentcroft.com>
2019-07-03 21:14:47 +0100

    session-010 remote script for building clean clone

--- bfg.cmd
+++ bfg.cmd
@@ -1,9 +1,32 @@
 
 
-set BFG=E:/dump/java/bfg-1.13.0.jar
+set -e
 
-%BFG% --delete-files truffit-report.xml
-%BFG% --delete-files truffit-report.txt
+:: https://rtyley.github.io/bfg-repo-cleaner/
+:: https://stackoverflow.com/questions/12450245/getting-a-working-copy-of-a-bare-repository
 
-git reflog expire --expire=now --all && git gc --prune=now --aggressive
+set git_url=file://e:/project/truffler/.git
+set BFG=java -jar bfg-1.13.0.jar
+
+::
+set replace_file=strings-to-replace.txt
+
+if exist %replace_file% (
+
+    git clone --mirror %git_url%
+
+    %BFG% --replace-text %replace_file% truffler.git
+
+    cd truffler.git
+
+    git reflog expire --expire=now --all
+    git gc --prune=now --aggressive
+
+    cd ..
+
+    git clone truffler.git
+
+) else (
+    echo "No strings to replace %replace_file%"
+)
 
